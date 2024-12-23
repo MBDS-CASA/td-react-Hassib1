ef11e12694f4feb251da7f732f09f9ac1ddfa363
Hassib1 <safaahassib202@gmail.com>
2024-12-23 16:42:59 +0100

    TD02 : Afficher l'année dans le Footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const Footer= () => {
-    return (
-        <footer style={{ backgroundColor: 'white', color: 'black', textAlign: 'center',}}>
-        <p>Tous droits réservés - Hassib Safaa</p>
-    </footer>
+    // Récupérer la date et l'heure actuelles
+    const currentDate = new Date();
+    
+    const year = currentDate.getFullYear();
+  
 
+    return (
+        <footer style={{ backgroundColor: 'white', color: 'black', textAlign: 'center',  }}>
+         
+            <p>{year} - Safaa Hassib, Tous droits réservés.</p>
+        </footer>
     );
 };
 
 export default Footer;
+
