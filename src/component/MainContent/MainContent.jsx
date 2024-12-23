0cb7dbfbae539e2433f13b0a9b4cd46067ea96eb
Hassib1 <safaahassib202@gmail.com>
2024-12-23 16:39:49 +0100

    TD02: Afficher la date et l'heure du jour

--- src/component/MainContent/MainContent.jsx
+++ src/component/MainContent/MainContent.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import React from 'react';
 
 const MainContent = () => {
+    // Récupérer la date et l'heure actuelles
+    const currentDate = new Date();
+    const day = currentDate.toLocaleString('fr-FR', { weekday: 'long' }); // Jour de la semaine
+    const month = currentDate.toLocaleString('fr-FR', { month: 'long' }); // Mois
+    const year = currentDate.getFullYear(); // Année
+    const hours = currentDate.getHours().toString().padStart(2, '0'); // Heure
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0'); // Minutes
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0'); // Secondes
+
     return (
-        <p>Ici, nous afficherons des informations interessantes </p>
-    )
-}
+      
+            <p>Bonjour, on est le {day}, {month}, {year} et il est {hours}:{minutes}:{seconds}</p>
+       
+    );
+};
 
 export default MainContent;
